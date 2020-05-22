887c4065e433b333b4d84461845194edf0e491bc
oboforty <rajmund.csombordi@gmail.com>
2020-05-22 19:41:38 +0200

    Test function

---
+++ R/discover.R
@@ -0,0 +1,14 @@
+
+#' Resolves missing metabolome IDs and other attributes in dataframe
+#'
+#' @param df.discovered A dataframe
+#' @return A list containing some statistics and the same dataframe, extended with missing data.
+#' @examples
+#' df_from_csv <- read.csv("meta_ids.csv", stringsAsFactors=FALSE)
+#' resp <- resolve_metabolites(df_from_csv)
+#' df.out <- resp$df
+#' view(df.out)
+resolve_metabolites <- function(df.discovered) {
+
+  return(123)
+}
