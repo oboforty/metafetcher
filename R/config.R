c8e492dcfec8efbcb4fd1799fbd3a390ba0b71ca
Sara Yones <sarayones@gmail.com>
2020-07-15 12:00:35 +0200

    Trial Docker4

--- R/config.R
+++ R/config.R
@@ -14,16 +14,16 @@ fileconf <- list(
   # download link:
   # https://hmdb.ca/system/downloads/current/hmdb_metabolites.zip
   # please unzip!!
-  hmdb_dump_file = "/tmp/hmdb_metabolites.xml",
+  hmdb_dump_file = "analysis/tmp/hmdb_metabolites.xml",
 
   # download link:
   # ftp://ftp.ebi.ac.uk/pub/databases/chebi/SDF/ChEBI_complete.sdf.gz
   # please uncompress!!
-  chebi_dump_file = "/tmp/ChEBI_complete.sdf",
+  chebi_dump_file = "analysis/tmp/ChEBI_complete.sdf",
 
   # download link:
   # https://www.lipidmaps.org/data/structure/download.php
   # please unzip!!
   # please rename the downloaded file -- if you chose to download it manually!
-  lipidmaps_dump_file = "/tmp/lipidmaps.sdf"
+  lipidmaps_dump_file = "analysis/tmp/lipidmaps.sdf"
 )
