3796ed3eed5347e7b0a18faa64fb9d733a9163fc
Sara Yones <sarayones@gmail.com>
2020-08-06 14:57:11 +0200

    trial global variables

--- R/db_ctx.R
+++ R/db_ctx.R
@@ -13,8 +13,8 @@ db.connect <- function (conf = NULL) {
 
   # connect to DB
   drv <- dbDriver("PostgreSQL")
-  mydb_conn <- dbConnect(drv, dbname = dbconf$dbname, host = dbconf$host, port = dbconf$port, user = dbconf$user, password = dbconf$password)
-  localis_connected <- TRUE
+  mydb_conn <<- dbConnect(drv, dbname = dbconf$dbname, host = dbconf$host, port = dbconf$port, user = dbconf$user, password = dbconf$password)
+  localis_connected <<- TRUE
 
   return (mydb_conn)
 }
