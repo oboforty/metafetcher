f76e45781b30334c24cebec2d07d02b41eec77a5
Sara Yones <sarayones@gmail.com>
2020-08-16 11:35:22 +0200

    trial config1 2

--- R/config1.R
+++ R/config1.R
@@ -1,12 +1,39 @@
 dbconf <- list(
-host = "localhost",
-port = 5432 ,
-db_name = "metafetcher" ,
-user = "postgres" ,
-password = "postgres"
+  host = "localhost",
+  #WithDocker
+  #  host = "172.17.0.3",
+  port = 5432,
+
+  dbname = "metafetcher",
+  user = "postgres",
+  password  = "postgres"
 )
+
+# If the files are present and the install script doesn't work
+# Try providing absolute paths, and also check the directory's permissions!
+# if you still want to work with relative paths, then try RStudio settings in: 'Session / Set Working Directory'
 fileconf <- list(
-hmdb_dump_file ="/Users/saryo614/metafetcher/tmp/hmdb_metabolites.xml",
-chebi_dump_file ="/Users/saryo614/metafetcher/tmp/ChEBI_complete.sdf",
-lipidmaps_dump_file ="/Users/saryo614/metafetcher/tmp/LMSD_20191002.sdf"
+  # download link:
+  # https://hmdb.ca/system/downloads/current/hmdb_metabolites.zip
+  # please unzip!!
+  hmdb_dump_file = "/Users/saryo614/metafetcher/tmp/hmdb_metabolites.xml",
+  #WithDocker
+  #temporarily disable
+  # hmdb_dump_file = "/tmp/hmdb_metabolites.xml",
+
+  # download link:
+  # ftp://ftp.ebi.ac.uk/pub/databases/chebi/SDF/ChEBI_complete.sdf.gz
+  # please uncompress!!
+  chebi_dump_file = "/Users/saryo614/metafetcher/tmp/ChEBI_complete.sdf",
+  #WithDocker
+  #temporarily disable
+  #chebi_dump_file = "/tmp/ChEBI_complete.sdf",
+
+  # download link:
+  # https://www.lipidmaps.org/data/structure/download.php
+  # please unzip!!
+  # please rename the downloaded file -- if you chose to download it manually!
+  lipidmaps_dump_file = "/Users/saryo614/metafetcher/tmp/LMSD_20191002.sdf"
+  #WithDocker
+  #lipidmaps_dump_file = "/tmp/LMSD_20191002.sdf"
 )
