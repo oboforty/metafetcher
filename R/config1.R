95b31bd9eef9829b69e54549a342a83dd230098d
Sara Yones <sarayones@gmail.com>
2020-08-17 11:31:53 +0200

    change config1 file back to project folder

--- R/config1.R
+++ R/config1.R
@@ -5,7 +5,7 @@ user ="postgres",
 password ="postgres"
 )
 fileconf <- list(
-hmdb_dump_file="/Users/saryo614/Desktop/tmp/hmdb_metabolites.xml",
-chebi_dump_file="/Users/saryo614/Desktop/tmp/chebi_dump_file.xml",
-lipidmaps_dump_file="/Users/saryo614/Desktop/tmp/LMSD_20191002.sdf"
+hmdb_dump_file="tmp/hmdb_metabolites.xml",
+chebi_dump_file="tmp/chebi_dump_file.xml",
+lipidmaps_dump_file="tmp/LMSD_20191002.sdf"
 )
