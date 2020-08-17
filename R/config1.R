ddfd0d22a0943452c6625c0fc24cf8f5c0036e2f
Sara Yones <sarayones@gmail.com>
2020-08-17 11:19:27 +0200

    config trial 7

--- R/config1.R
+++ R/config1.R
@@ -6,13 +6,8 @@ user = "postgres" ,
 password = "postgres"
 )
 fileconf <- list(
-<<<<<<< HEAD
-hmdb_dump_file ="tmp/hmdb_metabolites.xml",
-chebi_dump_file ="tmp/ChEBI_complete.sdf",
-lipidmaps_dump_file ="tmp/LMSD_20191002.sdf"
-=======
 hmdb_dump_file ="/Users/saryo614/Desktop/tmp/hmdb_metabolites.xml",
 chebi_dump_file ="/Users/saryo614/Desktop/tmp/ChEBI_complete.sdf",
 lipidmaps_dump_file ="/Users/saryo614/Desktop/tmp/LMSD_20191002.sdf"
->>>>>>> 05c1725001f8a4d230a62aafd24a3bcdeb9a7079
+
 )
