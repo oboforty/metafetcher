dea8b933476da64076d93e21b0e55e849abeac32
Sara Yones <sarayones@gmail.com>
2020-08-23 12:53:23 +0200

    create pubchem table

--- R/install.R
+++ R/install.R
@@ -110,6 +110,27 @@ install_databases <- function () {
 	ref_etc TEXT,
 	PRIMARY KEY (kegg_id)
 )"
+
+    db.query(SQL)
+    SQL="CREATE TABLE pubchem_data (
+	pubchem_id VARCHAR(20) NOT NULL,
+	names TEXT[],
+	mass FLOAT,
+	weight FLOAT,
+	monoisotopic_mass FLOAT,
+	logp FLOAT,
+	smiles TEXT[],
+	inchi TEXT,
+	inchikey VARCHAR(27),
+	formula VARCHAR(256),
+	chebi_id VARCHAR(20),
+	kegg_id VARCHAR(20),
+	hmdb_id VARCHAR(20),
+	chemspider_id VARCHAR(20),
+	ref_etc TEXT,
+	PRIMARY KEY (pubchem_id)
+)"
+
     db.query(SQL)
       db.commit()
     db.disconnect()
