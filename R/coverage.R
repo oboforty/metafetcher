d5b0640a148c994632b6d950e0b676064354de2b
Sara Yones <sarayones@gmail.com>
2020-11-13 14:43:12 +0100

    trial coverage 5

--- R/coverage.R
+++ R/coverage.R
@@ -20,7 +20,7 @@ do_consistency_test <- function (db, n,attempts) {
     print("This is the step i am in now:")
     print(j)
   db_tag <- paste(c(db,'_id'),collapse="")
-    records <- db.query(sprintf("SELECT %s FROM %s_data LIMIT %s OFFSET 6000", db_tag, db, n))
+    records <- db.query(sprintf("SELECT %s FROM %s_data LIMIT %s OFFSET 10", db_tag, db, n))
 
   resolve.options$suppress <- TRUE
   resolve.options$open_connection <- FALSE
